02acfb16d095c9adb44797ff1ad8f401e649f9fe
skoenig <soeren.koenig@mailbox.org>
2025-07-01 08:23:21 +0200

    style: formatting only

--- pipeline_metrics.py
+++ pipeline_metrics.py
@@ -16,17 +16,17 @@ from google.auth.exceptions import DefaultCredentialsError
 
 def pipeline_time(project_id=None, build_id=None):
     if project_id is None:
-        if os.environ.get("PROJECT_ID"):
-            project_id = os.environ.get("PROJECT_ID")
+        if os.environ.get('PROJECT_ID'):
+            project_id = os.environ.get('PROJECT_ID')
         else:
-            print("metric pipeline_time requires PROJECT_ID", file=sys.stderr)
+            print('metric pipeline_time requires PROJECT_ID', file=sys.stderr)
             return
 
     if build_id is None:
-        if os.environ.get("BUILD_ID"):
-            build_id = os.environ.get("BUILD_ID")
+        if os.environ.get('BUILD_ID'):
+            build_id = os.environ.get('BUILD_ID')
         else:
-            print("metric pipeline_time requires BUILD_ID", file=sys.stderr)
+            print('metric pipeline_time requires BUILD_ID', file=sys.stderr)
             return
 
     try:
@@ -35,16 +35,16 @@ def pipeline_time(project_id=None, build_id=None):
         pipeline_time = (
             datetime.datetime.now(tz=datetime.timezone.utc) - build.start_time
         )
-        print(f"pipeline_time: {pipeline_time.total_seconds()}")
+        print(f'pipeline_time: {pipeline_time.total_seconds()}')
     except DefaultCredentialsError:
         print(
-            "metric pipeline_time requires Application Default Credentials",
+            'metric pipeline_time requires Application Default Credentials',
             file=sys.stderr,
         )
         return
 
 
-def lead_time(deploy_tag_pattern="*", start_date=None, repo_path=None, tag_name=None):
+def lead_time(deploy_tag_pattern='*', start_date=None, repo_path=None, tag_name=None):
     # Traverse only through the revision history from a certain cutoff time,
     # otherwise examining large repositories will take significant time.
     if start_date is None:
@@ -52,17 +52,17 @@ def lead_time(deploy_tag_pattern="*", start_date=None, repo_path=None, tag_name=
         start_date = int(cutoff.timestamp())
 
     if repo_path is None:
-        if os.environ.get("REPO_PATH"):
-            repo_path = os.environ.get("REPO_PATH")
+        if os.environ.get('REPO_PATH'):
+            repo_path = os.environ.get('REPO_PATH')
         else:
-            print("metric lead_time requires REPO_PATH", file=sys.stderr)
+            print('metric lead_time requires REPO_PATH', file=sys.stderr)
             return
 
     if tag_name is None:
-        if os.environ.get("TAG_NAME"):
-            tag_name = os.environ.get("TAG_NAME")
+        if os.environ.get('TAG_NAME'):
+            tag_name = os.environ.get('TAG_NAME')
         else:
-            print("metric lead_time requires TAG_NAME", file=sys.stderr)
+            print('metric lead_time requires TAG_NAME', file=sys.stderr)
             return
 
     run = mk_run(repo_path)
@@ -76,9 +76,9 @@ def lead_time(deploy_tag_pattern="*", start_date=None, repo_path=None, tag_name=
     )
     if deployment_data_filtered:
         lead_time = datetime.datetime.now().timestamp() - deployment_data_filtered[1]
-        print(f"lead_time: {lead_time:.6f}")
+        print(f'lead_time: {lead_time:.6f}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     lead_time()
     pipeline_time()
